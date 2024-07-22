f2d08a913a8112489accd13ef9f3b7cfbe3bee8c
etienne_sagon <ejbs1@st-andrews.ac.uk>
2024-07-22 18:19:31 +0100

    re structure of the data directory to match the one in the working nodes + added patch metadata in file name

---
+++ utils.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+global_dir = Path("D:/CAMELYON16/data")
+
+#training slides
+training_dir = global_dir / "training/"
+train_normal_slide_path = training_dir / "normal/"
+train_tumor_slide_path = training_dir / "tumor/"
+train_annotation_path = training_dir / "lesion_annotations/"
+
+#testing slides
+testing_dir = global_dir / "testing/"
+test_slide_path = testing_dir / "images/"
+test_annotation_path = testing_dir / "lesion_annotations/"
+
+#validation slides
+validation_dir = global_dir / "validation/"
+val_normal_slide_path = validation_dir / "normal/"
+val_tumor_slide_path = validation_dir / "tumor/"
+val_annotation_path = validation_dir / "lesion_annotations/"
+
+#patche classification
+patch_classification_dir =  global_dir / "patch_classification/"
+
+#training patches
+train_patch_dir = patch_classification_dir / "train"
+train_normal_patch_path = train_patch_dir / "normal/"
+train_tumor_patch_path = train_patch_dir / "tumor/"
+train_normal_patch_index = 0
+train_tumor_patch_index = 0
+
+#validation patches
+val_patch_dir = patch_classification_dir / "val"
+val_normal_patch_path = val_patch_dir / "normal/"
+val_tumor_patch_path = val_patch_dir / "tumor/"
+val_normal_patch_index = 0
+val_tumor_patch_index = 0
+
+mag_level = 5
+patch_size = 299
+nb_patches_per_box = 5
